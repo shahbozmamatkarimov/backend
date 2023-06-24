394773e5dbeb5ae7389a4a10baef0d2433136830
Dilshod1421 <dilshod7861@gmail.com>
2023-06-25 00:23:26 +0500

    enableCors true

--- src/main.ts
+++ src/main.ts
@@ -10,6 +10,7 @@ async function testSystem() {
   try {
     const PORT = process.env.API_PORT;
     const app = await NestFactory.create(AppModule);
+    app.enableCors();
     app.useGlobalPipes(new ValidationPipe());
     app.use(cookieParser());
     app.setGlobalPrefix('api');
